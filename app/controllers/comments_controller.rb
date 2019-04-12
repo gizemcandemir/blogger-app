e676656104d836bb03c779bdbac2f8d50906641b
Gizem Candemir <gizemc@gmail.com>
2019-04-12 13:52:38 +0200

    finished blog comments feature

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,51 @@
+class CommentsController < ApplicationController
+  include CommentsHelper
+
+  def index
+    @comments = Comment.all
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+    @comment = Comment.new
+    @comment.comment_id = @comment.id
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.article_id = params[:article_id]
+    @comment.save
+
+    redirect_to article_path(@comment.article)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:author_name, :body)
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+
+    flash.notice = "Comment '#{@comment.title}' Updated!"
+
+    redirect_to comment_path(@comment)
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    flash.notice = "Comment '#{@comment.title}' Deleted!"
+
+    redirect_to comments_path(@comment)
+  end
+end
